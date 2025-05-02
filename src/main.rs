16582f0d84bfac41719e50662317115b62e9ab04
Tring-Er <tringalieros@gmail.com>
2025-05-02 17:05:03 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@
  * Version with creatures = 63.4920%
  */
 
-use reqwest;
 use std;
+
+use serde_json::Value;
+use reqwest;
 
 #[derive(Debug)]
 struct Node {
+    #[allow(dead_code)]
     gini_impurity: Option<f32>,
     feature_index: Option<usize>,
     on_true: Option<Box<Node>>,
@@ -177,77 +180,100 @@ fn generate_nodes(
 
 
 fn main() {
-    const N: usize = 1_000_000;
-    std::thread::Builder::new().stack_size(std::mem::size_of::<f64>() * 700 * N).spawn(|| {
-    let mut page_html: String = String::new();
     let mut html_decks: Vec<String> = Vec::new();
     let mut ranks: Vec<u64> = Vec::new();
-    let mut counter: usize = 1;
-    let urls: Vec<&str> = Vec::from(["https://www.mtgo.com/decklist/pauper-challenge-32-2025-04-0412763152", "https://www.mtgo.com/decklist/pauper-challenge-32-2025-04-0512763169", "https://www.mtgo.com/decklist/pauper-challenge-32-2025-04-0612763187", "https://www.mtgo.com/decklist/pauper-challenge-32-2025-04-1112765765", "https://www.mtgo.com/decklist/pauper-challenge-32-2025-04-1212765782", "https://www.mtgo.com/decklist/pauper-challenge-32-2025-04-1812769888", "https://www.mtgo.com/decklist/pauper-challenge-32-2025-04-1912769905", "https://www.mtgo.com/decklist/pauper-challenge-32-2025-04-2012769922", "https://www.mtgo.com/decklist/pauper-challenge-32-2025-04-2512772646", "https://www.mtgo.com/decklist/pauper-challenge-32-2025-04-2612772667"]);
-    for urls_index_sets in [vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]] {
-        html_decks = Vec::new();
-    for url_index in urls_index_sets{
-        println!("Requesting n: {:?}", counter);
-        counter += 1;
-        std::thread::sleep(std::time::Duration::from_secs(10));
-    let client: reqwest::blocking::Client = match reqwest::blocking::ClientBuilder::new().build() {
-        Ok(client_settings) => client_settings,
-        _ => panic!("Client settings inccorrect"),
-    };
-    let url: &str = urls[url_index];
-    let response: Result<reqwest::blocking::Response, reqwest::Error> = client
-.get(url)
-        .header("Host", "www.mtgo.com")
-        .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0")
-        .header("Accetp", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-        .header("Accept-Language", "en-Us,en;q=0.5")
-        .header("Accept-Encoding", "gzip, deflate, br, zstd")
-        .header("Referer", "https://www.mtgo.com/decklists?filter=Pauper")
-        .header("Sec-GPC", "1")
-        .header("Connection", "keep-alive")
-        .header("Cookie", "locale=en_US; tarteaucitron=!dgcMultiplegtagUa=wait; JSESSIONID=6653A133A2D8F719BD04FA8E26D6D75D.lvs-foyert1-3409")
-        .header("Upgrade-Insecure-Requests", "1")
-        .header("Sec-Fetch-Dest", "document")
-        .header("Sec-Fetch-Mode", "navigate")
-        .header("Sec-Fetch-Site", "same-origin")
-        .header("Sec-Fetch-User", "?1")
-        .header("Priority", "u=0, i")
-        .send();
-    if let Ok(resp) = response {
-        let mut page_html: String = resp.text().expect("failed");
-
-        if let (Some(first_index), Some(second_index)) = (page_html.find(
-            r#"window.MTGO.decklists.data = "#
-        ),
-        page_html.find(
-            r#"window.MTGO.decklists.type = "#
-        )) {
-            page_html = page_html[first_index + 29..second_index - 6].to_string();
-        }
-        
-        let end_decks_index: usize;
-        if let Some(index) = page_html.find("\"brackets\":") {
-            end_decks_index = index;
-        } else {
-            panic!("End decks index not found");
-        }
-
-        let unfiltered_decks: Vec<&str> = page_html[..end_decks_index].split(r#""loginid":""#).collect();
-        let mut login_ids: Vec<&str> = Vec::new();
-        for unfiltered_deck in &unfiltered_decks {
-            let login_id_end_index: usize = match unfiltered_deck.find("\"") {
-               Some(value) => value,
-               None => panic!("Value not found"),
-            };
-            let login_id: &str = &unfiltered_deck[..login_id_end_index];
-            login_ids.push(login_id);
-        }
-        println!("Request recived with: {:?} games", unfiltered_decks.len() - 1);
-        ranks.append(&mut extract_ranks(&page_html));
-        for deck_index in 1..unfiltered_decks.len() {
-            html_decks.push(unfiltered_decks[deck_index].to_string());
-        }
+    const MTGO_URI: &str = "https://www.mtgo.com/decklist/pauper-challenge-32-2025-04-";
+    let mut urls: Vec<String> = Vec::new();
+    const URL_QUERIES: [&str; 10] = [
+        "0412763152",
+        "0512763169",
+        "0612763187",
+        "1112765765",
+        "1212765782",
+        "1812769888",
+        "1912769905",
+        "2012769922",
+        "2512772646",
+        "2612772667",
+    ];
+    for url_query in URL_QUERIES {
+        urls.push(format!("{}{}", MTGO_URI, url_query));
     }
+    for url_index in 0..URL_QUERIES.len() {
+        println!("Requesting url: {:?}", urls[url_index]);
+        std::thread::sleep(std::time::Duration::from_secs(10));
+        let client: reqwest::blocking::Client = reqwest::blocking::Client::new();
+        let url: &str = &urls[url_index];
+        let response: Result<reqwest::blocking::Response, reqwest::Error> = client
+    .get(url)
+            .header("Host", "www.mtgo.com")
+            .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0")
+            .header("Accetp", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+            .header("Accept-Language", "en-Us,en;q=0.5")
+            .header("Accept-Encoding", "gzip, deflate, br, zstd")
+            .header("Referer", "https://www.mtgo.com/decklists?filter=Pauper")
+            .header("Sec-GPC", "1")
+            .header("Connection", "keep-alive")
+            .header("Cookie", "locale=en_US; tarteaucitron=!dgcMultiplegtagUa=wait; JSESSIONID=F0AA131DE3DABC64E264F93494E869F9.lvs-foyert2-3409")
+            .header("Upgrade-Insecure-Requests", "1")
+            .header("Sec-Fetch-Dest", "document")
+            .header("Sec-Fetch-Mode", "navigate")
+            .header("Sec-Fetch-Site", "same-origin")
+            .header("Sec-Fetch-User", "?1")
+            .header("Priority", "u=0, i")
+            .send();
+        if let Ok(resp) = response {
+            let mut page_html: String = resp.text().expect("failed");
+
+            if let (Some(first_index), Some(second_index)) = (page_html.find(
+                r#"window.MTGO.decklists.data = "#
+            ),
+            page_html.find(
+                r#"window.MTGO.decklists.type = "#
+            )) {
+                page_html = page_html[first_index + 29..second_index - 6].to_string();
+            }
+            
+            let end_decks_index: usize;
+            if let Some(index) = page_html.find("\"brackets\":") {
+                end_decks_index = index;
+            } else {
+                panic!("End decks index not found");
+            }
+
+            let unfiltered_decks: Vec<&str> = page_html[..end_decks_index].split(r#""loginid":""#).collect();
+            let mut login_ids: Vec<&str> = Vec::new();
+            for unfiltered_deck in &unfiltered_decks {
+                let login_id_end_index: usize = match unfiltered_deck.find("\"") {
+                   Some(value) => value,
+                   None => panic!("Value not found"),
+                };
+                let login_id: &str = &unfiltered_deck[..login_id_end_index];
+                login_ids.push(login_id);
+            }
+            println!("Request recived with: {:?} games", unfiltered_decks.len() - 1);
+            const STANDIGS_KEY: &str = "standings";
+            let parsed_data: Value = match serde_json::from_str(&page_html) {
+                Ok(value) => value,
+                Err(error) => panic!("Unable to parse json: {:?}", error),
+            };
+            let standings = match parsed_data[STANDIGS_KEY].as_array() {
+                Some(value) => Vec::from_iter(value),
+                None => panic!("Unable to find key: \"{:?}\"", STANDIGS_KEY),
+            };
+            let mut tmp_ranks: Vec<u64> = Vec::new();
+            for object in standings {
+                if let Value::String(value) = &object["rank"] {
+                    if let Ok(rank_value) = value.parse::<u64>() {
+                        ranks.push(rank_value);
+                    }
+                }
+            }
+            ranks.append(&mut tmp_ranks);
+            for deck_index in 1..unfiltered_decks.len() {
+                html_decks.push(unfiltered_decks[deck_index].to_string());
+            }
+        }
     }
     println!("Html decks are: {:?}", html_decks.len());
     let mut has_white_data: Vec<bool> = Vec::new();
@@ -258,7 +284,6 @@ fn main() {
     let mut tmp_lands_count_data: Vec<u8> = Vec::new();
     let mut tmp_creatures_count_data: Vec<u8> = Vec::new();
     let mut deck_position_less_than_9_data: Vec<bool> = Vec::new();
-    let random_scoring: [u8; 7] = [1, 1, 2, 5, 8, 13, 21];
     println!("{:?}, {:?}", html_decks.len(), ranks.len());
     for html_deck_index in 0..html_decks.len() {
         deck_position_less_than_9_data.push(ranks[html_deck_index] < 8);
@@ -313,128 +338,102 @@ fn main() {
                         };
                     }
                 }
+                }
             }
+            has_white_data.push(has_white_mana);
+            has_blue_data.push(has_blue_mana);
+            has_black_data.push(has_black_mana);
+            has_red_data.push(has_red_mana);
+            has_green_data.push(has_green_mana);
+            tmp_lands_count_data.push(lands_count);
+            tmp_creatures_count_data.push(creatures_count);
         }
-        has_white_data.push(has_white_mana);
-        has_blue_data.push(has_blue_mana);
-        has_black_data.push(has_black_mana);
-        has_red_data.push(has_red_mana);
-        has_green_data.push(has_green_mana);
-        tmp_lands_count_data.push(lands_count);
-        tmp_creatures_count_data.push(creatures_count);
-    }
 
 
 
-    let mut unique_lands_values: Vec<u8> = Vec::new();
-    for land_amount in &tmp_lands_count_data {
-        let mut contains_value: bool = false;
-        for unique_value in &unique_lands_values {
-            if *land_amount == *unique_value {
-                contains_value = true;
-                break;
-            }
-        }
-        if !contains_value {
-            unique_lands_values.push(*land_amount);
-        }
-    }
-    let mut unique_creatures_values: Vec<u8> = Vec::new();
-    for creatures_amount in &tmp_creatures_count_data {
-        let mut contains_value: bool = false;
-        for unique_value in &unique_creatures_values {
-            if *creatures_amount == *unique_value {
-                contains_value = true;
-                break;
-            }
-        }
-        if !contains_value {
-            unique_creatures_values.push(*creatures_amount);
-        }
-    }
-
-    println!("0:W, 1:U, 2:B, 3:R, 4:G");
-    println!("Unique lands values: {:?}", unique_lands_values);
-    println!("Unique creatures values: {:?}", unique_creatures_values);
-    let mut lands_count_data: Vec<Vec<bool>> = Vec::new();
-    for unique_value in unique_lands_values {
-        let mut unique_value_data: Vec<bool> = Vec::new();
+        let mut unique_lands_values: Vec<u8> = Vec::new();
         for land_amount in &tmp_lands_count_data {
-            let tmp: bool = *land_amount < unique_value;
-            unique_value_data.push(tmp);
-        }
-        lands_count_data.push(unique_value_data);
-    }
-    let mut creatures_count_data: Vec<Vec<bool>> = Vec::new();
-    for unique_value in unique_creatures_values {
-        let mut unique_value_data: Vec<bool> = Vec::new();
-        for creatures_amount in &tmp_creatures_count_data {
-            let tmp: bool = *creatures_amount < unique_value;
-            unique_value_data.push(tmp);
-        }
-        creatures_count_data.push(unique_value_data);
-    }
-
-
-
-    let training_percentage: f32 = 0.8;
-    let mut data_array_map: Vec<Vec<bool>> = Vec::from([
-        has_white_data[0..(has_white_data.len() as f32 * training_percentage) as usize].to_owned(),
-        has_blue_data[0..(has_blue_data.len() as f32 * training_percentage) as usize].to_owned(),
-        has_black_data[0..(has_black_data.len() as f32 * training_percentage) as usize].to_owned(),
-        has_red_data[0..(has_red_data.len() as f32 * training_percentage) as usize].to_owned(),
-        has_green_data[0..(has_green_data.len() as f32 * training_percentage) as usize].to_owned(),
-    ]);
-    data_array_map.append(&mut lands_count_data[0..][0..(lands_count_data.len() as f32 * training_percentage) as usize].to_vec());
-    data_array_map.append(&mut creatures_count_data[0..][0..(creatures_count_data.len() as f32 * training_percentage) as usize].to_vec());
-    let generated_nodes: Node = generate_nodes(
-        &Vec::from_iter(0..(deck_position_less_than_9_data.len() as f32 * training_percentage) as usize),
-        &data_array_map,
-        &deck_position_less_than_9_data[0..(deck_position_less_than_9_data.len() as f32 * training_percentage) as usize].to_vec(),
-    );
-    println!("Node: {:?}", generated_nodes);
-    let mut correct_predictions: usize = 0;
-    let mut total_predictions: usize = 0;
-    for index in (deck_position_less_than_9_data.len() as f32 * training_percentage) as usize..deck_position_less_than_9_data.len() - 1 {
-        total_predictions += 1;
-        let mut vector_data: Vec<bool> = Vec::from([has_white_data[index], has_blue_data[index], has_black_data[index], has_red_data[index], has_green_data[index]]);
-        for land_data in &lands_count_data {
-            vector_data.push(land_data[index]);
-        }
-        for creature_data in &creatures_count_data {
-            vector_data.push(creature_data[index]);
-        }
-        let prediction: bool = evaluate_data(&generated_nodes, vector_data);
-        println!("The prediction for data is: {:?}", prediction);
-        if prediction == deck_position_less_than_9_data[index] {
-            correct_predictions += 1;
-        }
-    }
-    println!("Prediction %: {:?}\nCorect: {:?}\nTotal: {:?}", correct_predictions as f32 / total_predictions as f32 * 100.0, correct_predictions, total_predictions);
-    }
-    }).unwrap().join().unwrap();
-}
-
-
-use serde_json::Value;
-
-fn extract_ranks(json_data: &str) -> Vec<u64> {
-    const STANDIGS_KEY: &str = "standings";
-    let parsed_data: Value = match serde_json::from_str(json_data) {
-        Ok(value) => value,
-        Err(error) => panic!("Unable to parse json: {:?}", error),
-    };
-    let standings = match parsed_data[STANDIGS_KEY].as_array() {
-        Some(value) => Vec::from_iter(value),
-        None => panic!("Unable to find key: \"{:?}\"", STANDIGS_KEY),
-    };
-    let mut ranks: Vec<u64> = Vec::new();
-    for object in standings {
-        if let Value::String(value) = &object["rank"] {
-            if let Ok(rank_value) = value.parse::<u64>() {
-                ranks.push(rank_value);
+            let mut contains_value: bool = false;
+            for unique_value in &unique_lands_values {
+                if *land_amount == *unique_value {
+                    contains_value = true;
+                    break;
+                }
+            }
+            if !contains_value {
+                unique_lands_values.push(*land_amount);
             }
         }
-    }
-    return ranks;
+        let mut unique_creatures_values: Vec<u8> = Vec::new();
+        for creatures_amount in &tmp_creatures_count_data {
+            let mut contains_value: bool = false;
+            for unique_value in &unique_creatures_values {
+                if *creatures_amount == *unique_value {
+                    contains_value = true;
+                    break;
+                }
+            }
+            if !contains_value {
+                unique_creatures_values.push(*creatures_amount);
+            }
+        }
+
+        println!("0:W, 1:U, 2:B, 3:R, 4:G");
+        println!("Unique lands values: {:?}", unique_lands_values);
+        println!("Unique creatures values: {:?}", unique_creatures_values);
+        let mut lands_count_data: Vec<Vec<bool>> = Vec::new();
+        for unique_value in unique_lands_values {
+            let mut unique_value_data: Vec<bool> = Vec::new();
+            for land_amount in &tmp_lands_count_data {
+                let tmp: bool = *land_amount < unique_value;
+                unique_value_data.push(tmp);
+            }
+            lands_count_data.push(unique_value_data);
+        }
+        let mut creatures_count_data: Vec<Vec<bool>> = Vec::new();
+        for unique_value in unique_creatures_values {
+            let mut unique_value_data: Vec<bool> = Vec::new();
+            for creatures_amount in &tmp_creatures_count_data {
+                let tmp: bool = *creatures_amount < unique_value;
+                unique_value_data.push(tmp);
+            }
+            creatures_count_data.push(unique_value_data);
+        }
+
+
+
+        let training_percentage: f32 = 0.8;
+        let mut data_array_map: Vec<Vec<bool>> = Vec::from([
+            has_white_data[0..(has_white_data.len() as f32 * training_percentage) as usize].to_owned(),
+            has_blue_data[0..(has_blue_data.len() as f32 * training_percentage) as usize].to_owned(),
+            has_black_data[0..(has_black_data.len() as f32 * training_percentage) as usize].to_owned(),
+            has_red_data[0..(has_red_data.len() as f32 * training_percentage) as usize].to_owned(),
+            has_green_data[0..(has_green_data.len() as f32 * training_percentage) as usize].to_owned(),
+        ]);
+        data_array_map.append(&mut lands_count_data[0..][0..(lands_count_data.len() as f32 * training_percentage) as usize].to_vec());
+        data_array_map.append(&mut creatures_count_data[0..][0..(creatures_count_data.len() as f32 * training_percentage) as usize].to_vec());
+        let generated_nodes: Node = generate_nodes(
+            &Vec::from_iter(0..(deck_position_less_than_9_data.len() as f32 * training_percentage) as usize),
+            &data_array_map,
+            &deck_position_less_than_9_data[0..(deck_position_less_than_9_data.len() as f32 * training_percentage) as usize].to_vec(),
+        );
+        println!("Node: {:?}", generated_nodes);
+        let mut correct_predictions: usize = 0;
+        let mut total_predictions: usize = 0;
+        for index in (deck_position_less_than_9_data.len() as f32 * training_percentage) as usize..deck_position_less_than_9_data.len() - 1 {
+            total_predictions += 1;
+            let mut vector_data: Vec<bool> = Vec::from([has_white_data[index], has_blue_data[index], has_black_data[index], has_red_data[index], has_green_data[index]]);
+            for land_data in &lands_count_data {
+                vector_data.push(land_data[index]);
+            }
+            for creature_data in &creatures_count_data {
+                vector_data.push(creature_data[index]);
+            }
+            let prediction: bool = evaluate_data(&generated_nodes, vector_data);
+            println!("The prediction for data is: {:?}", prediction);
+            if prediction == deck_position_less_than_9_data[index] {
+                correct_predictions += 1;
+            }
+        }
+        println!("Prediction %: {:?}\nCorect: {:?}\nTotal: {:?}", correct_predictions as f32 / total_predictions as f32 * 100.0, correct_predictions, total_predictions);
 }
